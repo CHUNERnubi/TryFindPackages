0e8ed5f6ddaa1a68cec14757db089f79b2d2bdc6
Wang, Chunyun <chunyun.wang@intel.com>
2025-07-25 14:32:44 +0800

    Support find llvm-project and execute mlir-tblgen tool in cmake
    * add one dialect.td as an example
    
    * update root CMakeList file to execute mlir-tblgen tool
    
    * support root CMakeList includes serveral key CMake commands
    
    * update README.md file for building commands
    
    Signed-off-by: Wang, Chunyun <chunyun.wang@intel.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TryFindPackages
+
+## Build Repo Commands
+
+```bash
+git clone <this_repo>
+cd TryFindPackages
+mkdir build && cd build
+cmake ..
+make -j4
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+// Include required MLIR headers
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "llvm/Support/raw_ostream.h"
 
+// Include generated files - they already contain the namespace declarations
+#include "dialect.hpp.inc"
+#include "dialect.cpp.inc"
 
 int main () {
     std::cout << "hello world" << std::endl;
